2ad797c975ca10e8c3739fe31b629fae0911bedc
Vanijya Technology <66530889+vanijyatechnology@users.noreply.github.com>
2025-09-26 15:44:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000; // ✅ Important for Railway
 
 app.get("/", (req, res) => {
-  res.send("Hello from Railway!");
+  res.send("Hnji bhai sahab chal gaye ");
 });
 
 app.listen(PORT, () => {
